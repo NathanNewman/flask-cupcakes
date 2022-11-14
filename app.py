542e7076244070f40912968db6d69bdf4dc59a02
Nathan Newman <lordnate.moose@gmail.com>
2022-11-14 14:30:11 -0600

    added database for heroku

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ import os
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','postgresql:///cupcakes')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'secret1')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'Secret!')
 
 debug = DebugToolbarExtension(app)
 
